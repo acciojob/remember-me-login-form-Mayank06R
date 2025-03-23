529fe9aded433f09e3912c2c7953c085e2508e3c
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-03-23 19:21:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//your JS code here. If required.
 document.addEventListener('DOMContentLoaded', function () {
     const loginForm = document.getElementById('loginForm');
     const existingUserButton = document.getElementById('existing');
-    
+
     // Display existing user login button if credentials are saved
     if (localStorage.getItem('username') && localStorage.getItem('password')) {
         existingUserButton.style.display = 'block';
@@ -14,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
         const rememberMe = document.getElementById('checkbox').checked;
-        
+
         alert(`Logged in as ${username}`);
 
         if (rememberMe) {
